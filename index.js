b276367ca41bc2e6c2e018ae2c67d02b3aee5760
hsz <jakub@chrzanowski.info>
2018-05-29 10:18:34 +0200

    prettier integration

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 const nspCheck = require('nsp/commands/check');
 
-function NodeSecurityPlugin (options) {
+function NodeSecurityPlugin(options) {
   this.options = options || {};
   this.options.reporter = this.options.reporter || 'summary';
 }
 
-NodeSecurityPlugin.prototype.apply = function (compiler) {
+NodeSecurityPlugin.prototype.apply = function(compiler) {
   var options = this.options;
   var errorCode = 0;
   var originalExit = process.exit;
-  var done = function (callback) {
+  var done = function(callback) {
     process.exit = originalExit;
     callback();
   };
 
-  compiler.plugin('emit', function (compilation, callback) {
-    process.exit = function (code) {
+  compiler.plugin('emit', function(compilation, callback) {
+    process.exit = function(code) {
       errorCode = errorCode || code;
     };
 
-    nspCheck.handler(options)
-      .then(function () {
-        switch (errorCode) {
-          case 0:
-            break;
-          case 1:
-            compilation.errors.push(new Error('Vulnerable packages found'));
-            break;
-          case 3:
-            compilation.errors.push(new Error('Internal nsp error'));
-            break;
-          default:
-            compilation.errors.push(new Error('Unhandled error'));
-        }
+    nspCheck.handler(options).then(function() {
+      switch (errorCode) {
+        case 0:
+          break;
+        case 1:
+          compilation.errors.push(new Error('Vulnerable packages found'));
+          break;
+        case 3:
+          compilation.errors.push(new Error('Internal nsp error'));
+          break;
+        default:
+          compilation.errors.push(new Error('Unhandled error'));
+      }
 
-        done(callback);
-      });
+      done(callback);
+    });
   });
 };
 
